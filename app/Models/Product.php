2ba2940f31bd59d41c04fb8856c40cf4d237c166
tyronemurg <tyronemurg@gmail.com>
2023-05-26 08:50:35 +0200

    Product Modeling

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    
     use HasFactory;
 
     protected $fillable = [
         'name', 'body', 'image', 'user_id'
     ];
-    
+
 
 }
